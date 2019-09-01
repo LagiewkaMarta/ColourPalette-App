0aa06fd1f98637d85667286979c042d234ae4562
Marta <lagiewka.marta.dev@gmail.com>
2019-09-01 05:00:43 +0200

    ADD DeleteIcon to MiniPalette

--- src/Components/MiniPalette.js
+++ src/Components/MiniPalette.js
@@ -1,22 +1,23 @@
-import React from "react";
-import { withStyles } from "@material-ui/styles";
-import styles from "../styles/MiniPaletteStyles";
+import React from 'react';
+import DeleteIcon from "@material-ui/icons/Delete";
+import { withStyles } from '@material-ui/styles';
+import styles from '../styles/MiniPaletteStyles';
+
 function MiniPalette({ classes, paletteName, emoji, colors, id, handleClick }) {
-  const miniColorBoxes = colors.map(color => (
-    <div
-      className={classes.miniColor}
-      style={{ backgroundColor: color.color }}
-      key={color.name}
-    />
-  ));
-  return (
-    <div className={classes.root} onClick={() => handleClick(id)}>
-      <div className={classes.colors}>{miniColorBoxes}</div>
-      <h5 className={classes.title}>
-        {paletteName} <span className={classes.emoji}>{emoji}</span>
-      </h5>
-    </div>
-  );
+	const miniColorBoxes = colors.map(color => (
+		<div className={classes.miniColor} style={{ backgroundColor: color.color }} key={color.name} />
+	));
+	return (
+		<div className={classes.root} onClick={() => handleClick(id)}>
+			<div className={classes.delete}>
+				<DeleteIcon className={classes.deleteIcon} style={{ transition: 'all 0.3s ease-in-out' }} />
+			</div>
+			<div className={classes.colors}>{miniColorBoxes}</div>
+			<h5 className={classes.title}>
+				{paletteName} <span className={classes.emoji}>{emoji}</span>
+			</h5>
+		</div>
+	);
 }
 
 export default withStyles(styles)(MiniPalette);
