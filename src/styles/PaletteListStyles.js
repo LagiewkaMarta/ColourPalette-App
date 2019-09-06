b041ef2b1ecfce8f8ff4e6186aba27deb5d83efa
Marta <lagiewka.marta.dev@gmail.com>
2019-09-06 21:25:46 +0200

    MAKE Palette List responsive

--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,17 +1,26 @@
+import sizes from "../helpers/sizeHelpers";
+
 export default {
     root: {
       backgroundColor: "blue",
-      height: "100vh",
+      minHeight: "100vh",
       display: "flex",
-      // alignItems: "flex-start",
-      justifyContent: "center"
+      alignItems: "flex-start",
+      justifyContent: "center",
+      overflow: "scroll"
     },
     container: {
       width: "50%",
       display: "flex",
       flexDirection: "column",
       alignItems: "flex-start",
-      // flexWrap: "nowrap"
+      flexWrap: "wrap",
+      [sizes.down("xl")]: {
+        width: "80%"
+      },
+      [sizes.down("xs")]: {
+        width: "70%"
+      },
     }, 
     nav: {
       display: "flex",
@@ -30,6 +39,13 @@ export default {
       width: "100%",
       display: "grid",
       gridTemplateColumns: "repeat(3, 30%)",
-      gridGap: "5%"
+      gridGap: "1.5rem",
+      [sizes.down("md")]: {
+       gridTemplateColumns: "repeat(2, 50%)",
+      },
+      [sizes.down("xs")]: {
+       gridTemplateColumns: "repeat(1, 100%)",
+       gridGap: "1rem",
+      }
     }
   };
